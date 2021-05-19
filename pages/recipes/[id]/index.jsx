35bdbfb0230862cef1de7fede28b4f9a43f8818d
Matt-htims <matt.smith3108@gmail.com>
2021-05-19 15:41:48 +0100

    got rid of the text that said 'individual recipe' as it was not needed

--- pages/recipes/[id]/index.jsx
+++ pages/recipes/[id]/index.jsx
@@ -94,12 +94,6 @@ const IndividualRecipe = () => {
 			)}
 
 			<div className="w-11/12 m-auto space-y-0 md:space-y-12 max-w-7xl">
-				<h1
-					className="text-center text-3xl md:text-4xl text-darkBlue font-accent font-bold"
-					onClick={handleRecipeShow}
-				>
-					Individual recipe
-				</h1>
 				<div className="main-section space-y-6 md:space-y-0 md:space-x-5 md:flex">
 					<div className="md:w-1/4">
 						<Sidebar img={recipe.image} ingredients={recipe.ingredients} />
